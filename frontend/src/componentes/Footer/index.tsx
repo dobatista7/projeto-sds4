8b6d63e4ea196c5288eb53f7e90260baf0742254
Douglas Oliveira Batista <dobatista7@gmail.com>
2021-09-07 11:13:08 -0300

    Basic Static Components

---
+++ frontend/src/componentes/Footer/index.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/douglasobatista/" target="_blank" rel="noreferrer">Douglas Oliveira Batista</a></p>
+                <p className="text-light"><small><strong>Semana Spring React 4.0</strong><br />
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+
+    );
+}
+
+export default Footer;
